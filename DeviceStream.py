46be7683bd703f8bd2a7a85735d6d43ae1f2336f
simons <yahoochemical@126.com>
2024-09-29 00:01:38 +0800

    [New] optimize rec_gui.py using the class DeviceStream and MenuWindow.py

--- DeviceStream.py
+++ DeviceStream.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import threading
 from FileThread import FileWriting
+from AlgoProcess import AlgoProcess
+from ctypes import c_float 
 import time
 
 class DeviceStream:
@@ -14,18 +16,27 @@ class DeviceStream:
     def __init__(self, value=None):
         self.stream = None
         self.peak =0
-        self.device =0
+        self.input_device = sd.query_hostapis(sd.default.hostapi)['default_input_device']
+        self.output_device = sd.query_hostapis(sd.default.hostapi)['default_output_device']
         self.recording =0
-        self.audio_q = queue.Queue()
+        self.Info = ''
+        self.input_audio_q = queue.Queue()
         self.metering_q = queue.Queue(maxsize=1)
+        self.instance = AlgoProcess()
     def audio_callback(self, indata, frames, time, status):
         """This is called (from a separate thread) for each audio block."""
         if self.recording:
-            self.audio_q.put(indata.copy())
+            self.input_audio_q.put(indata.copy())
             self.previously_recording = True
+            c_float_array = indata.astype(np.ctypeslib.as_ctypes_type(c_float))
+            for j in range(self.stream.channels):
+                for i in range(self.stream.blocksize):
+                    self.instance.dataIn[j*self.stream.blocksize + i] = c_float_array[i,j]
+            self.instance.process()
+            self.angle = float(self.instance.dataOut[0])
         else:
             if self.previously_recording:
-                self.audio_q.put(None)
+                self.input_audio_q.put(None)
                 self.previously_recording = False
 
         self.peak = max(self.peak, np.max(np.abs(indata)))
@@ -35,14 +46,13 @@ class DeviceStream:
             pass
         else:
             self.peak = 0
-
-
-    def create_stream(self, device,):
+            
+    def create_stream(self, device,filename):
         if self.stream is not None:
             self.stream.abort()
             self.stream.stop()
             self.stream.close()
-        self.device = device
+        self.input_device = device
         print('start>>>>>>')
         #print(self.stream.samplerate)
         self.stream = sd.InputStream(
@@ -53,14 +63,15 @@ class DeviceStream:
         self.thread = threading.Thread(
             target=FileWriting.file_writing_thread,
             kwargs=dict(
-                file='test.wav',
+                file=filename,
                 mode='x',
                 samplerate=int(self.stream.samplerate),
                 channels=self.stream.channels,
-                q=self.audio_q,
+                q=self.input_audio_q,
             ),
         )
         self.thread.start()
+
     def stop_stream(self, *args):
         self.recording = False
         #self.wait_for_thread()
@@ -72,11 +83,13 @@ def main():
     root = tk.Tk()
     #root.withdraw()
     testMenu = MenuWindow.SettingsWindow(root,'test')
-    
 
     #root.mainloop()
+    filename ='out.wav'
     test = DeviceStream()
-    test.create_stream(testMenu.input_dev['index'])
+    print(test.input_device)
+    print(test.output_device)
+    test.create_stream(testMenu.input_dev['index'],filename)
     print(testMenu.input_dev['index'])
     print(testMenu.output_dev['index'])
     time.sleep(10)
--- rec_gui.py
+++ rec_gui.py
@@ -10,18 +10,12 @@ Blocking the file-writing thread for some time is no problem, as long as the
 recording can be stopped successfully when it is supposed to.
 
 """
-import queue
-import threading
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
-import numpy as np
-import sounddevice as sd
-from ctypes import c_float 
 from datetime import datetime
 
-from AlgoProcess import AlgoProcess
-from FileThread import FileWriting
+from DeviceStream import DeviceStream
 #from Led import LED
 import MenuWindow
 
@@ -30,9 +24,8 @@ class RecGui(tk.Tk):
   
     def __init__(self):
         super().__init__()
-        
-        self.device=0
-        self.instance = AlgoProcess()
+
+        self.manageStream = DeviceStream()
         self.title('SoundSimulation')
         self.geometry('1000x500') 
         
@@ -70,86 +63,23 @@ class RecGui(tk.Tk):
 
         # We try to open a stream with default settings first, if that doesn't
         # work, the user can manually change the device(s)
-        self.create_stream()
-        self.recording = self.previously_recording = False
-        self.audio_q = queue.Queue()
-        self.audio_temp = queue.Queue()
-        self.peak = 0
-        self.metering_q = queue.Queue(maxsize=1)
-
         self.protocol('WM_DELETE_WINDOW', self.close_window)
         self.init_buttons()
-        self.update_gui()
+        #self.update_gui()
 
     def on_settings(self, *args):
-        w = MenuWindow.SettingsWindow(self, 'Input Settings')
-        if(w.result!=None):
-            print(w.result)
-            #self.device =w.result
-            self.create_stream(device=w.result)
+        w = MenuWindow.SettingsWindow(self, 'Settings')
+        self.manageStream.input_device = w.input_dev_id
+        self.manageStream.output_device = w.output_dev_id
+        self.manageStream.Info = w.Info
 
     def generation_settings(self, *args):
         w = MenuWindow.Generate(self, 'Generate')
 
-    def create_stream(self, device=None,):
-        if self.stream is not None:
-            self.stream.abort()
-            self.stream.stop()
-            self.stream.close()
-        chan = 2
-        if 'USB' in sd.query_devices(self.device)['name']:
-            chan = max(16,sd.query_devices(device)['max_input_channels'])
-        else:
-            chan = 2
-        #chan = 2
-        print(self.device)
-        #print(self.stream.samplerate)
-        self.stream = sd.InputStream(
-            device=device, channels=chan, callback=self.audio_callback)
-        self.stream.start()
-        #sd.stream()
-
-    def audio_callback(self, indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
-        if self.recording:
-            self.audio_q.put(indata.copy())
-            self.previously_recording = True
-            c_float_array = indata.astype(np.ctypeslib.as_ctypes_type(c_float))
-            for j in range(self.stream.channels):
-                for i in range(self.stream.blocksize):
-                    self.instance.dataIn[j*self.stream.blocksize + i] = c_float_array[i,j]
-            self.instance.process()
-            self.angle = float(self.instance.dataOut[0])
-        else:
-            if self.previously_recording:
-                self.audio_q.put(None)
-                self.previously_recording = False
-
-        self.peak = max(self.peak, np.max(np.abs(indata)))
-        try:
-            self.metering_q.put_nowait(self.peak)
-        except queue.Full:
-            pass
-        else:
-            self.peak = 0
-
     def on_rec(self):
         self.recording = True
         filename = 'Sim_'+datetime.now().strftime('%Y_%m_%d_%H_%M_%S')+'.wav'
-        if self.audio_q.qsize() != 0:
-            print('WARNING: Queue not empty!')
-        self.thread = threading.Thread(
-            target=FileWriting.file_writing_thread,
-            kwargs=dict(
-                file=filename,
-                mode='x',
-                samplerate=int(self.stream.samplerate),
-                channels=self.stream.channels,
-                q=self.audio_q,
-            ),
-        )
-        self.thread.start()
-        # NB: File creation might fail!  For brevity, we don't check for this.
+        self.manageStream.create_stream(self.manageStream.input_device,filename)
         self.rec_button['text'] = 'stop'
         self.rec_button['command'] = self.on_stop
         self.rec_button['state'] = 'normal'
@@ -158,16 +88,7 @@ class RecGui(tk.Tk):
     def on_stop(self, *args):
         self.rec_button['state'] = 'disabled'
         self.recording = False
-        self.wait_for_thread()
-
-    def wait_for_thread(self):
-        self.after(10, self._wait_for_thread)
-
-    def _wait_for_thread(self):
-        if self.thread.is_alive():
-            self.wait_for_thread()
-            return
-        self.thread.join()
+        self.manageStream.stop_stream(self.manageStream.input_device)
         self.init_buttons()
 
     def init_buttons(self):
@@ -177,22 +98,21 @@ class RecGui(tk.Tk):
             self.rec_button['state'] = 'normal'
 
     def update_gui(self):
-        self.Info = 'Source device:\n'+sd.query_devices(self.device)['name']+'\tSampleRate:'+str(self.stream.samplerate)+'\tChannel Number.:'+str(self.stream.channels)
-        self.Info_label['text'] = self.Info
-        try:
-            peak = self.metering_q.get_nowait()
-        except queue.Empty:
-            pass
-        else:
-            self.meter['value'] = peak
+        self.Info_label['text'] = self.manageStream.Info
+        #try:
+        #    peak = self.metering_q.get_nowait()
+        #except queue.Empty:
+        #    pass
+        #else:
+        #    self.meter['value'] = peak
         content = 'DOA(Direction Of Arrival) Angle:'+str(round(self.angle/3.1415926*18)*10)
         self.angle_label['text'] = content
         #self.LedOBJ.LED_Control(int(self.angle/3.1415926*16))
         self.after(500, self.update_gui)
 
     def close_window(self):
-        if self.recording:
-            self.on_stop()
+        #if self.recording
+        #    self.on_stop()
         self.destroy()
 
 def main():
