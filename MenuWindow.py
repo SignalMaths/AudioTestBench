85e0ae8a060b57a7bb996e8df64bacea13848033
simons <yahoochemical@126.com>
2024-09-28 11:32:38 +0800

    [ENH] the device config in menu

--- MenuWindow.py
+++ MenuWindow.py
@@ -41,6 +41,7 @@ class SettingsWindow(Dialog):
     def body(self, master):
 
         # input device Frame
+        self.init =0
         self.FrameInputDevice = Frame(self)
         self.FrameInputDevice.pack(anchor='w')
         self.Info ='NONE'
@@ -49,15 +50,17 @@ class SettingsWindow(Dialog):
         self.input_hostapi_list.pack(anchor='w')
         self.input_hostapi_list['values'] = [
             hostapi['name'] for hostapi in sd.query_hostapis()]
+
         # select sound device
-        ttk.Label(self.FrameInputDevice, text='Select sound device:').pack(anchor='w')
+        ttk.Label(self.FrameInputDevice, text='Select input device:').pack(anchor='w')
         self.device_input_ids = []
         self.device_input_list = ttk.Combobox(self.FrameInputDevice, state='readonly', width=50)
         self.device_input_list.pack(anchor='w')
-        self.input_hostapi_list.bind('<<ComboboxSelected>>', self.update_device_list)
+        self.input_hostapi_list.bind('<<ComboboxSelected>>', self.update_input_hostapis_list)
         with contextlib.suppress(sd.PortAudioError):
             self.input_hostapi_list.current(sd.default.hostapi)
             self.input_hostapi_list.event_generate('<<ComboboxSelected>>')
+        self.device_input_list.bind('<<ComboboxSelected>>', self.update_input_dev)
 
         # output device for configure.
         self.FrameDeviceOut = Frame(self)
@@ -68,54 +71,51 @@ class SettingsWindow(Dialog):
         self.out_hostapi_list['values'] = [
             hostapi['name'] for hostapi in sd.query_hostapis()]
         # select sound device
-        ttk.Label(self.FrameDeviceOut, text='Select sound device:').pack(anchor='w')
+        ttk.Label(self.FrameDeviceOut, text='Select output device:').pack(anchor='w')
         self.device_ids = []
         self.device_list = ttk.Combobox(self.FrameDeviceOut, state='readonly', width=50)
         self.device_list.pack(anchor='w')
-        self.out_hostapi_list.bind('<<ComboboxSelected>>', self.update_Outdevice_list)
+        self.out_hostapi_list.bind('<<ComboboxSelected>>', self.update_Out_hostapi_list)
+        #self.device_list.bind('<<ComboboxSelected>>', self.update_Outdevice_list)
         with contextlib.suppress(sd.PortAudioError):
             self.out_hostapi_list.current(sd.default.hostapi)
             self.out_hostapi_list.event_generate('<<ComboboxSelected>>')
+        self.device_list.bind('<<ComboboxSelected>>', self.update_output_dev)
 
-        #self.Info_label1 = tk.Label(self,text='text', font=('Arial', 10),justify="left" ).pack(anchor='w')
         self.FrameInfo = Frame(self)
         self.FrameInfo.pack(anchor='w')
         self.Info_label = ttk.Label(self.FrameInfo,text=self.Info, font=('Arial', 10),justify="left" )
         self.Info_label.pack(anchor='w')
+        self.init =1
         self.update_gui()
 
-    def update_gui(self):
-        
-        str_input = 'Input device:\n' +self.shostapi['name']+ '\t'+sd.query_devices(self.input_dev)['name'] 
-        self.Info = str_input+ '\n'+'OutputDev:'
-        self.Info_label['text'] = self.Info
-        self.after(100, self.update_gui)
-
-        #self.Info_label1['text'] = 'self.Info'
-    def update_device_list(self, *args):
-        self.shostapi = sd.query_hostapis(self.input_hostapi_list.current())
-        print(self.shostapi['name'])
-#
+    def update_input_hostapis_list(self, *args):
+        self.input_shostapi = sd.query_hostapis(self.input_hostapi_list.current())
         DeviceType = 'max_input_channels'
         default_device = 'default_input_device'
         self.device_input_ids = [
             idx
-            for idx in self.shostapi['devices']
+            for idx in self.input_shostapi['devices']
             if sd.query_devices(idx)[DeviceType] > 0]
         self.device_input_list['values'] = [
             sd.query_devices(idx)['name'] for idx in self.device_input_ids]
-        default = self.shostapi[default_device]
+        default = self.input_shostapi[default_device]
         if default >= 0:
             self.device_input_list.current(self.device_input_ids.index(default))
-        self.input_dev = self.device_input_list.current()
-        print(self.device_input_ids)
-        print(self.input_dev)
-        print(sd.query_devices(self.input_dev)['name'])
+        self.input_dev_id = self.device_input_ids[self.device_input_list.current()]
+        self.input_dev=sd.query_devices(self.input_dev_id)
+        if(self.init ==1):
+            self.update_gui()
 
-    def update_Outdevice_list(self, *args):
+    def update_input_dev(self, *args):
+        self.input_shostapi = sd.query_hostapis(self.input_hostapi_list.current())
+        self.input_dev_id = self.device_input_list.current()
+        self.input_dev=sd.query_devices(self.device_input_ids[self.input_dev_id])
+        if(self.init ==1):
+            self.update_gui()
+
+    def update_Out_hostapi_list(self, *args):
         self.out_hostapi = sd.query_hostapis(self.out_hostapi_list.current())
-        print(self.out_hostapi['name'])
-
         DeviceType = 'max_output_channels'
         default_device = 'default_output_device'
         self.device_ids = [
@@ -127,7 +127,36 @@ class SettingsWindow(Dialog):
         default = self.out_hostapi[default_device]
         if default >= 0:
             self.device_list.current(self.device_ids.index(default))
-        self.output_dev = self.device_list.current()            
+        self.output_dev_id = self.device_ids[self.device_list.current()]
+        self.output_dev=sd.query_devices(self.output_dev_id)
+        if(self.init ==1):
+            self.update_gui()
+        
+    def update_output_dev(self, *args):
+        self.output_dev_id = self.device_list.current()
+        self.output_dev=sd.query_devices(self.device_ids[self.output_dev_id])
+        if(self.init ==1):
+            self.update_gui()
+
+    def update_gui(self):
+        
+        str_input = 'Input device:\n' +self.input_shostapi['name']+ '\t'+self.input_dev['name']
+        srt_output = 'Output device:\n' +self.out_hostapi['name']+ '\t'+self.output_dev['name']
+        self.Info = str_input+ '\n'+srt_output
+        #print('update gui ==================')
+        #print('>>>>>>>>>>>input device')
+        #print(self.input_shostapi['name'])
+        #print(self.device_input_ids)
+        #print(self.input_dev['name'])
+        #print('>>>>>>>>>>>output device')
+        #print(self.out_hostapi['name'])
+        #print(self.device_ids)
+        #print(self.output_dev['name'])
+        ##print(self.Info)
+        #print('update gui>>>>>>>>>>>>>>>>>')
+        self.Info_label['text'] = self.Info
+        #self.after(100, self.update_gui)
+
 
     def validate(self):
         self.result = self.device_ids[self.device_list.current()]
